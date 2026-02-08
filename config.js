16458ebc4d0cd105d6109883fb3b4a840710686c
webkaty808ff <154459572+webkaty808ff@users.noreply.github.com>
2026-02-09 03:02:37 +0600

    Update config.js

--- config.js
+++ config.js
@@ -49,8 +49,8 @@ const CONFIG = {
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yay! I'm the luckiest person in the world! My Dinara 🎉💝💖💝💓",
+        message: "Now come get your махабат! Бейби, 14 февраль 19:00 I'll wait for you !!!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
