cfe5eaf66953386b03a5d2337c5f7b11fd83039e
webkaty808ff <154459572+webkaty808ff@users.noreply.github.com>
2026-02-09 02:59:15 +0600

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dxgge2atw/video/upload/v1770583831/The_Walters_-_I_love_you_so__SkySound.cc_knfxxu.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
